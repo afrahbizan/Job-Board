35b8ef4e9396a648f7a7e57839738060c51a211e
afrahbezan <afrah_bez@yahoo.com>
2024-12-10 09:10:15 +0200

    9.Creating the Job Board Project and Job Model

---
+++ my-job-board/database/migrations/2024_12_09_104038_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->String('title');
+            $table->text('description');
+            $table->String('location');
+            $table->unsignedInteger('salary');
+            $table->enum('experience', Job::$experience);
+            $table->enum('category', Job::$category);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
