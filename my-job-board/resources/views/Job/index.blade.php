d90f2d08e6e28757682fbb84d7089eb46b181c91
afrahbezan <afrah_bez@yahoo.com>
2024-12-16 22:18:59 +0200

    Job Page: Job Card & Link Button Components

--- my-job-board/resources/views/Job/index.blade.php
+++ my-job-board/resources/views/Job/index.blade.php
@@ -1,32 +1,15 @@
 <div>
     <x-layout>
+
         @foreach ($jobs as $job)
-            <x-card class='mb-2'>
-                <div class=' mb-4 flex justify-between'>
-                    <h2 class="text-lg font-medium">
-                        {{ $job->title }}
-                    </h2>
-                    <div class="text-slate-500">
-                        ${{ number_format($job->salary) }}</div>
-                </div> 
-                <div class="mb-4 flex justify-between text-sm text-slate-500">
-                 <div class="flex space-x-4">
-                   <div >company name:</div>
-                   <div>{{$job->location }}</div>
-                  </div>
-                 <div class="flex space-x-4"> 
-                   <x-tag>{{ Str::ucfirst($job->experience)}}</x-tag>
-                   <x-tag>{{ $job->category }}</x-tag>
-                  
+            <x-job-card class="mb-4" :$job>
+                <div>
+                    <x-link-button :href="route('jobs.show', $job)">show</x-link-button>
                 </div>
-                 
-                </div>
-                <div class='text-sm text-slate-500'>
-                  {!! nl2br(e($job->description) )!!}
-                </div>
-            
-            </x-card>
+
+            </x-job-card>
         @endforeach
+
     </x-layout>
 
 </div>
