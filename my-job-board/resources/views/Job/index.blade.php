761435bf944177e5621ace585b183f3b628df521
afrahbezan <afrah_bez@yahoo.com>
2024-12-17 10:28:20 +0200

    breadcrumbs

--- my-job-board/resources/views/Job/index.blade.php
+++ my-job-board/resources/views/Job/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     <x-layout>
-
+<x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]"/>
         @foreach ($jobs as $job)
             <x-job-card class="mb-4" :$job>
                 <div>
--- my-job-board/resources/views/Job/show.blade.php
+++ my-job-board/resources/views/Job/show.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-breadcrumbs  class="mb-4" :links="['jobs'=>route('jobs.index'),$job->title=>'#']"/>
     <x-job-card :$job>
 
     </x-job-card>
