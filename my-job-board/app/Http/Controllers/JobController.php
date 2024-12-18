e607353eb1ef1299833b3910c0e4f4c225fda833
afrahbezan <afrah_bez@yahoo.com>
2024-12-18 11:27:23 +0200

    Filtering Jobs: Min & Max Salary

--- my-job-board/app/Http/Controllers/JobController.php
+++ my-job-board/app/Http/Controllers/JobController.php
@@ -13,14 +13,24 @@ class JobController extends Controller
     public function index()
     {
 
-        $jobs=Job::query();
+        $jobs = Job::query();
 
-        $jobs->when(request('search'),function($query){
-          $query->where('title','like','%'. request('search'). '%') 
-               ->orWhere('description', 'like', '%' . request('search') . '%');
+ 
+
+        $jobs->when(request('search'), function ($query) {
+            $query->where(function ($query) {
+                $query->where('title', 'like', '%' . request('search') . '%')
+                    ->orWhere('description', 'like', '%' . request('search') . '%');
+            });
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
         });
-        
-        return view ('job.index',["jobs"=>$jobs->get()]);
+
+
+
+        return view('job.index', ["jobs" => $jobs->get()]);
     }
 
     /**
@@ -44,7 +54,7 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-       return view('job.show',compact('job'));
+        return view('job.show', compact('job'));
     }
 
     /**
